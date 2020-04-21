277173cbd4d76406c96dc572595d088f95589d1e
Trung-Jeager-2019 <trungjeager2020@Dell-Precision-5510.localdomain>
2020-04-21 16:06:59 +0700

    change assets to static

--- spamsystem/settings.py
+++ spamsystem/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
